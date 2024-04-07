56a902cff3640816cc446523d6e654e67c836ffb
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-07 16:55:05 +0300

    add extends keyword

---
+++ src/ChineseChef.java
@@ -0,0 +1,6 @@
+public class ChineseChef extends  Chef{
+
+    public  void makeFriedRice(){
+        System.out.println("The chef makes fried rice");
+    }
+}
---
+++ src/ItalianChef.java
@@ -0,0 +1,13 @@
+public class ItalianChef extends  Chef{
+
+
+    public  void makePasta(){
+        System.out.println("The chef makes pasta");
+    }
+
+    public void makeSpecialDish(){
+        System.out.println("The chef makes eggplant parm");
+    }
+
+
+}
